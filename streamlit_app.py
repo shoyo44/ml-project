86270ce96945de721c60415128eff7ef4e8abf64
Dhanush S <dhanush4102005@gmail.com>
2025-05-10 12:33:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ with st.expander('Data'):
   data=pd.read_csv("https://raw.githubusercontent.com/shoyo44/ml-project/refs/heads/master/penguins_data.csv")
   data
   st.write('**Dependent Data (X)**')
-  X_raw=data.drop(['Species'],axis=0)
+  X_raw=data.drop('Species',axis=1)
 
