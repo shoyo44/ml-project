5702554f131824acef598676ed08e0f051cb0eed
Dhanush S <dhanush4102005@gmail.com>
2025-05-10 12:24:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 st.title('Machine learning app')
 st.info('Predicts species about penguins')
-with st.expander('Data') as:
+with st.expander('Data'):
   st.write('**Raw Data**')
-  data=https://raw.githubusercontent.com/shoyo44/ml-project/refs/heads/master/penguins_data.csv
+  data=pd.read_csv("https://raw.githubusercontent.com/shoyo44/ml-project/refs/heads/master/penguins_data.csv")
   data
 
