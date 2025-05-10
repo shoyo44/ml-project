d65b4fc253eea9585f60c3a4eb891091f90926ad
Dhanush S <dhanush4102005@gmail.com>
2025-05-10 14:38:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,6 @@ with st.expander('Input Features'):
   input_penguins
 encode=['island','sex']
 df_penguins=pd.get_dummies(input_penguins,prefix=encode)
-df_penguins=df_penguins.drop('sex_Female',axis=1)
 
 with st.expander('Data preparation'):
   input_a=df_penguins[:1]
