25662b7a8a3058601546b4818dddfc801ecc713d
Dhanush S <dhanush4102005@gmail.com>
2025-05-10 14:22:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ dff={'island':island,
     'body_mass_g':body_mass_g,
     'bill_depth_mm':bill_depth_mm,
     'sex':sex}
-input_data=pd.DataFrame(dff)
+input_data=pd.DataFrame(dff,index=[1])
 input_penguins=pd.concat([input_data,X_raw],axis=0)
 with st.expander('Input Features'):
   st.write("**Input Data**")
